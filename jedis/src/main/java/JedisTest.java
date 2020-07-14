98a66f73255b7e84a6bb5290ef8c3d68475043eb
mask <mask@ksam.com>
2020-07-14 19:33:49 +0800

    hotfix add println

--- jedis/src/main/java/JedisTest.java
+++ jedis/src/main/java/JedisTest.java
@@ -10,8 +10,10 @@ public class JedisTest {
         JedisPool jedisPool = new JedisPool("39.104.97.71", 6379);
         Jedis jedis = jedisPool.getResource();
 
+        System.out.println("mask");
         String ping = jedis.ping();
         System.out.println(ping);
         jedisPool.close();
+
     }
 }
